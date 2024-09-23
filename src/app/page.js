028c5314b9980920ec8c7abad78be1c7dbf06609
Cody <66153585+nonstickcode@users.noreply.github.com>
2024-09-23 11:40:26 -0700

    style adjust to header

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <header className="text-8xl font-semibold text-gray-500">
+      <header className="text-6xl lg:text-8xl font-semibold text-gray-500 mt-3">
         <h1>#RYDEBLK</h1>
       </header>
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
