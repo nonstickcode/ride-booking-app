d12948afdd879418c8acefe5d743d1f09d9d8bb2
Cody <66153585+nonstickcode@users.noreply.github.com>
2024-09-23 12:33:08 -0700

    working well

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,30 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <header className="text-6xl lg:text-8xl font-semibold text-gray-500 mt-3">
+    <div className="flex flex-col min-h-screen items-center justify-center px-4">
+      <header className="text-5xl font-semibold text-gray-500">
         <h1>#RYDEBLK</h1>
       </header>
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
+
+      <main className="mt-2 flex flex-col items-center space-y-6">
+        <p className="text-lg font-semibold text-gray-500">With Jamie</p>
+
+        {/* Image with fixed width and height */}
+        <Image
+          className="rounded-lg"
+          src="/images/escalade.jpg"
+          width={240}
+          height={240}
+          alt="escalade"
+        />
+
+        {/* BookingForm component with the same width as the image */}
         <BookingForm />
       </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6"></footer>
+
+      <footer className="mt-8 flex items-center justify-center space-x-4">
+        {/* Footer content can go here */}
+      </footer>
     </div>
   );
 }
