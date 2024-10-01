2c630164724bbc71a11bf0de00374a53f55a5303
Cody <66153585+nonstickcode@users.noreply.github.com>
2024-10-01 13:56:56 -0700

    remove hover on image scale

--- src/components/RideImage.js
+++ src/components/RideImage.js
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export default function RideImage() {
   return (
     <Image
-      className="mb-4 transform rounded-lg border transition-transform hover:scale-105"
+      className="mb-4 transform rounded-lg"
       src="/images/jamies-escalade.jpeg"
       width={300}
       height={200}
