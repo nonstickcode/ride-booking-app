f667f1bc1e1cec08a49056bca1db16eb0b6bd985
Cody <66153585+nonstickcode@users.noreply.github.com>
2024-10-03 17:54:15 -0700

    hover link gray 500

--- src/components/PlacesAutocomplete.js
+++ src/components/PlacesAutocomplete.js
@@ -87,7 +87,7 @@ const PlacesAutocomplete = ({ setSelected, label }) => {
           href={generateGoogleMapsLink()}
           target="_blank"
           rel="noopener noreferrer"
-          className="text-sm text-gray-300 hover:text-blue-500"
+          className="text-sm text-gray-300 hover:text-gray-400"
         >
           Use Google Maps
         </a>
