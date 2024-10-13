6c17badf2607ab9b92bc999edc5617b044a583da
Cody <66153585+nonstickcode@users.noreply.github.com>
2024-10-12 22:09:22 -0700

    delete old dateUtils file not used anymore

--- src/utils/dateUtils.js
+++
@@ -1,9 +0,0 @@
-// utils/dateUtils.js
-
-export const combineDateAndTime = (date, time) => {
-  const combinedDate = new Date(date);
-  combinedDate.setHours(time.getHours());
-  combinedDate.setMinutes(time.getMinutes());
-  combinedDate.setSeconds(time.getSeconds());
-  return combinedDate;
-};
