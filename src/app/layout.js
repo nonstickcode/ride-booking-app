644dd93b5710fdb0323856b23cb59fb27d71bace
Cody <66153585+nonstickcode@users.noreply.github.com>
2024-12-17 12:10:24 -0700

    fix safe zone for iphone

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Existing Favicons */}
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -28,6 +29,11 @@ export default function RootLayout({ children }) {
         />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+
+        {/* Add iOS meta tags for Home Screen */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="theme-color" content="#000000" />
       </head>
       <body>{children}</body>
     </html>
