44a4359aa36889edfba28cc6d55a45a1de0e924f
Cody <66153585+nonstickcode@users.noreply.github.com>
2024-12-16 20:51:50 -0700

    working with env file locally

--- src/app/api/calendar/checkAvailability/route.js
+++ src/app/api/calendar/checkAvailability/route.js
@@ -2,15 +2,21 @@ import { google } from 'googleapis';
 import { NextResponse } from 'next/server';
 
 // Safely load service account credentials from the environment variable
-let credentials;
-try {
-  credentials = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON);
-} catch (error) {
-  console.error('Error parsing GOOGLE_SERVICE_ACCOUNT_JSON:', error.message);
-  throw new Error(
-    'Invalid or missing GOOGLE_SERVICE_ACCOUNT_JSON environment variable.'
-  );
-}
+const credentials = {
+  type: "service_account",
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+  universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+};
+
+
 
 // Access the Calendar ID from environment variables
 const CALENDAR_ID = process.env.GOOGLE_CALENDAR_ID;
