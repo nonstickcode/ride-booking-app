0144bdd6537144d8924f22c4b552749f30e84a87
Cody <66153585+nonstickcode@users.noreply.github.com>
2024-12-19 12:07:17 -0700

    adjust footer position and font sizes

--- src/app/page.js
+++ src/app/page.js
@@ -160,11 +160,11 @@ function HomeContent() {
         )}
 
         {/* Footer */}
-        <footer className="absolute bottom-0 left-0 right-0 text-center pb-4">
-          <p className="text-lg text-gray-200">or text 310-947-9464</p>
+        <footer className="absolute bottom-0 left-0 right-0 flex flex-col items-center space-y-2 pb-4">
+          <p className="text-lg text-gray-300">or text 310-947-9464</p>
           <a
             href="/privacypolicy"
-            className="mt-2 block text-gray-200 hover:text-blue-300"
+            className="text-sm font-light text-gray-500 transition-colors duration-300 hover:text-gray-400"
           >
             Privacy Policy
           </a>
